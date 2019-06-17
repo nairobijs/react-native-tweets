566e3e24bf53bf51351f91d348b296490fbfafca
MilkBiscuit <dc9909@hotmail.com>
2019-06-17 20:46:57 +1200

    Fix the lowercase issue of '.app/Store'

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import store from './app/store'
+import store from './app/Store'
 import Tweets from './app/tweets'
 
 type Props = {};
